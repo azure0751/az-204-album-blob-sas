e3e1bd866b03cf151aadfea3959356c9eea5897b
Azure0751 <58366593+abhi.0751@users.noreply.github.com>
2024-09-18 10:23:49 +0530

    final

--- Blob-SAS-album/Controllers/HomeController.cs
+++ Blob-SAS-album/Controllers/HomeController.cs
@@ -19,44 +19,93 @@ namespace Blob_SAS_album.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var images = await GetImagesAsync();
+            var (images, errorMessage) = await GetImagesAsync();
+            ViewBag.ErrorMessage = errorMessage;
             return View(images);
         }
 
-        private async Task<IEnumerable<string>> GetImagesAsync()
+        //private async Task<IEnumerable<string>> GetImagesAsync()
+        //{
+        //    var images = new List<string>();
+
+        //    string sasToken = _configuration["AzureBlobStorage:SasToken"];
+        //    string containerName = _configuration["AzureBlobStorage:ContainerName"];
+        //    string storageAccountName = _configuration["AzureBlobStorage:StorageAccount"];
+
+        //    if (string.IsNullOrWhiteSpace(sasToken))
+        //    {
+        //        throw new InvalidOperationException("SAS Token must be provided.");
+        //    }
+
+        //    string blobServiceUri = $"https://{storageAccountName}.blob.core.windows.net";
+        //    var blobServiceClient = new BlobServiceClient(new Uri($"{blobServiceUri}?{sasToken}"));
+
+        //    // Get a reference to the container
+        //    var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+        //    // List and print all blob URLs in the container
+        //    Console.WriteLine($"Listing blobs in container '{containerName}':");
+
+        //    var blobs = containerClient.GetBlobsAsync();
+        //    await foreach (var blobItem in containerClient.GetBlobsAsync())
+        //    {
+        //        var blobClient = containerClient.GetBlobClient(blobItem.Name);
+        //        var blobUrl = blobClient.Uri.ToString();
+        //        images.Add(blobUrl);
+        //        Console.WriteLine(blobUrl); // Print blob URL
+        //    }
+
+           
+        //    return images;
+        //}
+
+        private async Task<(IEnumerable<string>, string)> GetImagesAsync()
         {
             var images = new List<string>();
-
             string sasToken = _configuration["AzureBlobStorage:SasToken"];
             string containerName = _configuration["AzureBlobStorage:ContainerName"];
             string storageAccountName = _configuration["AzureBlobStorage:StorageAccount"];
 
             if (string.IsNullOrWhiteSpace(sasToken))
             {
-                throw new InvalidOperationException("SAS Token must be provided.");
+                var errorMessage = "SAS Token must be provided. Please set the environment variable 'AzureBlobStorage__SasToken' or update the 'SasToken' value in the 'appsettings.json'.";
+                return (images, errorMessage);
             }
 
-            string blobServiceUri = $"https://{storageAccountName}.blob.core.windows.net";
-            var blobServiceClient = new BlobServiceClient(new Uri($"{blobServiceUri}?{sasToken}"));
-
-            // Get a reference to the container
-            var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
-
-            // List and print all blob URLs in the container
-            Console.WriteLine($"Listing blobs in container '{containerName}':");
-
-            var blobs = containerClient.GetBlobsAsync();
-            await foreach (var blobItem in containerClient.GetBlobsAsync())
+            if (string.IsNullOrWhiteSpace(containerName))
             {
-                var blobClient = containerClient.GetBlobClient(blobItem.Name);
-                var blobUrl = blobClient.Uri.ToString();
-                images.Add(blobUrl);
-                Console.WriteLine(blobUrl); // Print blob URL
+                var errorMessage = "containerName must be provided. Please set the environment variable 'AzureBlobStorage__ContainerName' or update the 'ContainerName' value in the 'appsettings.json'.";
+                return (images, errorMessage);
             }
 
-           
+            if (string.IsNullOrWhiteSpace(storageAccountName))
+            {
+                var errorMessage = "storageAccountName must be provided. Please set the environment variable 'AzureBlobStorage__StorageAccount' or update the 'storageAccountName' value in the 'appsettings.json'.";
+                return (images, errorMessage);
+            }
 
-            return images;
+            try
+            {
+                string blobServiceUri = $"https://{storageAccountName}.blob.core.windows.net";
+                var blobServiceClient = new BlobServiceClient(new Uri($"{blobServiceUri}?{sasToken}"));
+                var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+                var blobs = containerClient.GetBlobsAsync();
+                 await foreach (var blobItem in containerClient.GetBlobsAsync())
+                {
+                var blobClient = containerClient.GetBlobClient(blobItem.Name);
+                       var blobUrl = blobClient.Uri.ToString();
+                       images.Add(blobUrl);
+                       Console.WriteLine(blobUrl); // Print blob URL
+                 }
+            }
+            catch (Exception ex)
+            {
+                var errorMessage = $"Error accessing Blob Storage: {ex.Message}";
+                return (images, errorMessage);
+            }
+
+            return (images, null);
         }
     }
 }
--- Readme.md
+++ Readme.md
@@ -1,1 +1,20 @@
-This is readme.md file to show
+
+If application is working fine then you will see the output like this.
+![](Tutorial-Images/AlbumPage.png)
+
+If SAS token is not specified in App settings/Environment variables. 
+![](Tutorial-Images/no-sas.png)
+
+If StorageAccount is not specified in App settings/Environment variables. 
+![](Tutorial-Images/no-storage.png)
+
+If StorageAccount is not specified in App settings/Environment variables. 
+![](Tutorial-Images/no-container.png)
+
+If any error occurred.You will see error message.
+![](Tutorial-Images/Error.png)
+
+
+
+
+
